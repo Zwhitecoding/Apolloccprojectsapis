1898bcad02eae62590a43fab7bf0e9a502ec664d
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-10-27 13:40:24 +0800

    Update fileio.js

--- routes/fileio.js
+++ routes/fileio.js
@@ -11,7 +11,7 @@ const uploadHeaders = { Accept: 'application/json, text/plain, */*' };
 module.exports.routes = {
   name: "Fileio Uploader",
   desc: "Uploads files to file.io from a given URL",
-  category: "Uploader file API",
+  category: "File Management",
   usages: "/api/fileio",
   query: "?url=",
   method: "get",
