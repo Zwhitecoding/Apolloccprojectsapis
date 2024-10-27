d865b4c2cbd46cb4b4fe975a9514c99e1c2fba82
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-10-27 14:26:14 +0800

    Update upscale.js

--- routes/upscale.js
+++ routes/upscale.js
@@ -10,22 +10,28 @@ module.exports.routes = {
   desc: "Upscales images using specified model. Choose model 1 or 2.",
   category: "Tools",
   usages: "/api/upscale",
-  query: "?url=&model=1",
+  query: "?url=<encodedURL>&model=1",
   method: "get",
 };
 
 module.exports.onAPI = async (req, res) => {
-  const { url, model = "1" } = req.originalUrl.split('/api/upscale?url=&model=1')[1];;
+  let { url, model = "1" } = req.query;
+
   if (!url) {
     return res.status(400).json({ error: "Image URL is required." });
   }
 
   try {
+    
+    url = decodeURIComponent(url);
+
+
     const data = await upscale(url, model);
     const response = await axios.get(data.image_url, { responseType: "arraybuffer" });
     const tempFilePath = path.join(__dirname, "temp_upscaled_image.jpg");
     fs.writeFileSync(tempFilePath, response.data);
 
+  
     res.sendFile(tempFilePath, (err) => {
       if (err) {
         res.status(500).json({ error: "File sending failed.", details: err.message });
